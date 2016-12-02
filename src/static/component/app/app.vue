8d1f1e93bb0f6f743181fc23ec5199167589985d
riotkkwok <riotkkwok@gmail.com>
2016-12-02 21:22:28 +0800

    [ADD] 新增组件

---
+++ src/static/component/app/app.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: 'app',
+    components: {
+        'wHeader': require('header'),
+        'wEditor': require('../component/editor/editor.js'),
+        'wTimeTable': require('../component/timeTable/timeTable.js'),
+        'wFooter': require('footer'),
+        'wLoginPanel': require('loginPanel')
+    }
+});
+</script>
