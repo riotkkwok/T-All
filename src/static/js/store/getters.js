a160764c00e695ff45792fd18f1cdb33bddd097a
Rio Kwok <riotkkwok@gmail.com>
2016-12-11 07:51:36 +0800

    [ADD] 添加公共状态

--- src/static/js/store/getters.js
+++ src/static/js/store/getters.js
@@ -9,3 +9,22 @@ export const loginInfo = (state) => {
 export const showLoginPanel = (state) => {
     return state.showLoginPanel;
 }
+
+export const taskList = (state) => {
+    return state.taskList;
+}
+
+export const startDate = (state) => {
+    return state.startDate;
+}
+
+export const nowDate = (state) => {
+    return state.nowDate;
+}
+
+export const endDate = (state) => {
+    const y = state.nowDate ? 
+        state.nowDate.getFullYear() : state.startDate ? 
+        state.startDate.getFullYear() : (new Date()).getFullYear();
+    return new Date(y+'/12/31');
+}
