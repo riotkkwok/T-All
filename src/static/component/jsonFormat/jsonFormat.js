c33bce97ba15a255bdb05288c49e32622de95a46
Rio Kwok <riotkkwok@gmail.com>
2016-12-12 14:38:13 +0800

    [ADD] 添加jsonFormat组件

---
+++ src/static/component/jsonFormat/jsonFormat.js
@@ -0,0 +1,97 @@
+import resp from 'resp';
+
+const types = {
+    obj: 'object',
+    arr: 'array',
+    num: 'number',
+    str: 'string',
+    bool: 'boolean',
+    func: 'function'
+}
+
+const check = (r, o, ko) => {
+    let result = {};
+    if(o instanceof Array){
+        return check4Array(r, o);
+    }
+    for(let k in r){
+        if(o.hasOwnProperty(k)){
+            if(getType(r[k]) === getType(o[k])){
+                if(getType(r[k]) === types.obj){
+                    result[k] = check(r[k], o[k]);
+                }else if(getType(r[k]) === types.arr){
+                    result[k] = check4Array(r[k], o[k]);
+                }else{
+                    result[k] = o[k];
+                }
+            }else{
+                throw new Error('Unmatch data type.');
+            }
+        }
+    }
+    if(JSON.stringify(result) === '{}' && !ko){
+        result = undefined;
+    }
+    return result;
+}
+
+const check4Array = (r, o) => {
+    let result = [], tmp;
+    if(o.length === 0){
+        return o;
+    }
+    for(let i=0, j=0; i<o.length; i++){
+        if(getType(r[0]) === getType(o[i])){
+            if(getType(o[i]) === types.obj){
+                tmp = check(r[0], o[i])
+                if(tmp !== undefined){
+                    result[j++] = tmp;
+                }
+            }else if(getType(o[i]) === types.arr){
+                tmp = check4Array(r[0], o[i]);
+                if(tmp !== undefined){
+                    result[j++] = tmp;
+                }
+            }else{
+                result[j++] = o[i];
+            }
+        }else{
+            throw new Error('Unmatch data type.');
+        }
+    }
+    if(JSON.stringify(result) === '[]'){
+        result = undefined;
+    }
+    return result;
+}
+
+const getType = (o) => {
+    return Object.prototype.toString.call(o).toLowerCase().replace(/(\[object )|(\])/g, '');
+}
+
+export const validate = (name, o) => {
+    const obj;
+    if(typeof o === 'object'){
+        obj = o;
+    }else if(typeof o === 'string'){
+        try{
+            obj = JSON.parse(o);
+        }catch(e){
+            console.error(e);
+            return false;
+        }
+    }else{
+        console.error('Incorrect data type.');
+        return false;
+    }
+    if(!resp[name]){
+        console.error('Incorrect api name.');
+        return false;
+    }
+    try{
+        return check(resp[name], obj);
+    }catch(e){
+        console.error(e);
+        return false;
+    }
+}
---
+++ src/static/component/jsonFormat/resp.js
@@ -0,0 +1,30 @@
+export default resp = {
+    getTaskList: {
+        code: 0,
+        data: {
+            taskList: [
+                {
+                    id: '', // 任务ID
+                    name: '', // 任务名
+                    asg: [ // 参与人员
+                        {
+                            id: '',
+                            effort: {
+                                dev: [''], // 开发时间
+                                intg: [''], // 联调自测时间
+                                test: [''], // 测试时间
+                                rls: '' // 上线时间
+                            }
+                        }
+                    ],
+                    pjm: '', // 项目经理
+                    pdm: '', // 产品经理
+                    dsg: '', // 美术设计
+                    wbe: '', // web后端
+                    tsr: '', // 测试人员
+                    remark: '' // 备注
+                }
+            ]
+        }
+    }
+}
