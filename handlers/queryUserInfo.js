89e4ca10fc4d36b186f371fba80a9fb663f154ca
Rio Kwok <riotkkwok@gmail.com>
2017-03-31 14:59:35 +0800

    [EDIT] 更新queryUserInfo

--- handlers/queryUserInfo.js
+++ handlers/queryUserInfo.js
@@ -1,17 +1,39 @@
-const handler = function(){
+const crypto = require('crypto');
+
+const userModel = require('../services/connectDB.js').user;
+
+const handler = function(param){
     console.log('queryUserInfo handler');
+    const now = Date.now();
+    const that = this;
     return function(cb){
-        /* mock response [start] */
-        const resp = {
-            "code": 0,
-            "data": {
-                "userId": "001",
-                "userName": "陈大文",
-                "adminId": "A1"
+        const ticket = that.cookies.get('NSESSIONID');
+
+        if(!param || !param.userId || !ticket){
+            cb({
+                msg: 'invalid request parameters.'
+            }, {});
+        }
+
+        const pr = userModel.
+            find({userId: param.userId, ticket: ticket}).
+            where('ticketValid').gt(now).
+            limit(1).
+            exec();
+        pr.then(function(rs){
+            console.log('then');
+            console.log(rs);
+            const resp = {};
+            if(rs.length === 1){
+                console.log('got');
+                resp.userName = rs[0].userName;
+                resp.userId = rs[0].userId;
+                resp.adminId = rs[0].adminId;
+            }else{
+                console.log('queryUserInfo - no record');
             }
-        };
-        /* mock response [end] */
-        cb(null, JSON.stringify(resp));
+            cb(null, resp);
+        });
     }
 }
 
