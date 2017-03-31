67712c2f376b1b833a06b81b49c84a8ab39a3154
Rio Kwok <riotkkwok@gmail.com>
2017-03-31 19:21:39 +0800

    [EDIT] 引入koa-bodyparser，并加入异常处理

--- bin/app.js
+++ bin/app.js
@@ -1,4 +1,5 @@
 const koa = require('koa2');
+const bodyparser = require('koa-bodyparser');
 const co = require('co');
 const handlers = {
     queryUserInfo: require('../handlers/queryUserInfo.js'),
@@ -23,6 +24,8 @@ const handlers = {
 
 const app = new koa();
 
+app.keys = ['t-all secret 2017'];
+
 app.use(co.wrap(function *(ctx, next) {
     const start = new Date();
     yield next();
@@ -30,20 +33,29 @@ app.use(co.wrap(function *(ctx, next) {
     console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 }));
 
+app.use(bodyparser());
+
 // handlers
 app.use(co.wrap(function *(ctx, next) {
     const name = ctx.url.replace('/', '').split('?')[0];
-    const param = ctx.query;
+    const param = ctx.request.body;
     console.log(name);
     console.log(param);
     if(handlers.hasOwnProperty(name)){
-        const data = yield handlers[name].apply(this, param);
+        ctx.response.type = 'application/json';
         const body = {
             "code": 0,
-            "data": data
+            "data": {}
         }
-        ctx.body = JSON.stringify(body);
-        ctx.response.type = 'application/json';
+        try{
+            const data = yield handlers[name].call(ctx, param);
+            body.code = 0;
+            body.data = data;
+        }catch(e){
+            console.log(e);
+        }finally{
+            ctx.body = JSON.stringify(body);
+        }
     }else{
         ctx.body = 'Koa 2 : 404 - Not Found';
     }
