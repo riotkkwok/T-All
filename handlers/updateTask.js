f84ddbd2548e8e91289aa20a031f3ad0d1e9720e
Rio Kwok <riotkkwok@gmail.com>
2017-05-02 19:39:31 +0800

    [EDIT] 更新updateTask

--- handlers/updateTask.js
+++ handlers/updateTask.js
@@ -1,10 +1,27 @@
-const handler = function(){
+const taskModel = require('../services/connectDB.js').task;
+
+const handler = function(param){
     console.log('updateTask handler');
+    const tid = param.id;
+    delete param.id;
+    delete param.color;
     return function(cb){
-        /* mock response [start] */
-        const resp = {};
-        /* mock response [end] */
-        cb(null, JSON.stringify(resp));
+        const pr = taskModel.
+            update({ id: tid }, param).
+            exec();
+        pr.then(function(rs){
+            const resp = {};
+            if(rs && rs.nModified === 1 && rs.ok === 1){
+                resp.result = 0;
+                cb(null, resp);
+            }else{
+                resp.result = -1;
+                cb({
+                    msg: 'failed to update task.',
+                    errCode: resp.result
+                }, {});
+            }
+        });
     }
 }
 
