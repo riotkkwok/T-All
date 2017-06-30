0ec460eebda3484a97eb54c7be9f02e7b8e79424
Rio Kwok <riotkkwok@gmail.com>
2017-06-30 20:27:11 +0800

    [ADD] 添加editLeave组件

---
+++ static/src/component/editLeave/editLeave.vue
@@ -0,0 +1,24 @@
+<template lang="jade" src="./editLeave.jade"></template>
+
+<style lang="sass" src="./editLeave.scss"></style>
+
+<script>
+import Vue from 'vue'
+
+export default {
+    created() {},
+    data() {
+        return {
+            ppl: {},
+            leave: {},
+            isErr: false,
+            errMsg: ''
+        }
+    },
+    methods: {
+        checkInput() {},
+        updateLeaves() {}
+    }
+};
+
+</script>
