0d54611dd2d9b336c30c0f89389aa4e640268eb9
Rio Kwok <riotkkwok@gmail.com>
2017-07-14 13:57:52 +0800

    [EDIT] 完成deleteLeave功能

--- handlers/deleteLeave.js
+++ handlers/deleteLeave.js
@@ -1,10 +1,33 @@
-const handler = function(){
+const assigneeModel = require('../services/connectDB.js').assignee;
+
+const handler = function(param){
     console.log('deleteLeave handler');
     return function(cb){
-        /* mock response [start] */
-        const resp = {};
-        /* mock response [end] */
-        cb(null, JSON.stringify(resp));
+        const pr = assigneeModel.
+            find({id: param.id}).
+            select('-_id leaves').
+            exec();
+        pr.then(function(rs){
+            const leaves = rs[0].leaves;
+            delete leaves[param.date];
+            const pr2 = assigneeModel.
+                update({id: param.id}, {$set: {leaves}}).
+                exec();
+            pr2.then(function(rs){
+                console.log(rs);
+                const resp = {};
+                if(rs && rs.nModified === 1 && rs.ok === 1){
+                    resp.result = 0;
+                    cb(null, resp);
+                }else{
+                    resp.result = -1;
+                    cb({
+                        msg: 'failed to delete leave.',
+                        errCode: resp.result
+                    }, {});
+                }
+            });
+        });
     }
 }
 
