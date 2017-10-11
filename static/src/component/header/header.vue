1ccd3a12b0026697f21033e8ae80906731baeba4
Rio Kwok <riotkkwok@gmail.com>
2017-10-11 19:42:31 +0800

    Revert "[EDIT[ 更新header设置菜单和UI"
    This reverts commit bc116d1a88f3056c2affaeba370d2b7c55ac231a.

--- static/src/component/header/header.vue
+++ static/src/component/header/header.vue
@@ -5,7 +5,6 @@ header
     div#username(@click="loginClick") 
         | {{ username || '游客' }}
     ul.settings-list(v-show="showSettingsList")
-        li.settings-item(v-show="isLogined && isAdmin" @click="assigneeManage") 参与者面板
         li.settings-item(v-show="!isLogined" @click="loginClick") 登录
         li.settings-item(v-show="isLogined" @click="logoutClick") 退出
 </template>
@@ -47,7 +46,6 @@ a.logo {
     width: 100px;
     border: 1px solid $color1;
     border-radius: 3px;
-    padding: 5px;
     background-color: $bgColor1;
     cursor: pointer;
     z-index: 5;
@@ -65,7 +63,6 @@ a.logo {
 .settings-item {
     @extend %ui-font1;
     text-align: center;
-    padding: 2px;
 }
 </style>
 
@@ -85,9 +82,6 @@ module.exports = {
         isLogined() {
             return this.isLoginStatusReady && !!this.$store.getters['loginInfo'].userName;
         },
-        isAdmin() {
-            return this.$store.getters['isAdmin'];
-        },
         username() {
             return this.$store.getters['loginInfo'].userName;
         }
@@ -98,11 +92,6 @@ module.exports = {
         },
         settingsClick() {
             this.showSettingsList = !this.showSettingsList;
-        },
-        assigneeManage() {
-            if(this.$store.getters['isLogined'] && this.$store.getters['isAdmin']){
-                this.$router.push('/assignee');
-            }
         },
         loginClick() {
             if(this.isLogined){
